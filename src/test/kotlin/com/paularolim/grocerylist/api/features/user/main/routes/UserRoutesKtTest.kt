945165736eab545837143e6b76601f59a9d351d4
Paula Rolim <rolim.paula1999@gmail.com>
2024-09-02 18:52:09 -0300

    test: should return 201 on register

---
+++ src/test/kotlin/com/paularolim/grocerylist/api/features/user/main/routes/UserRoutesKtTest.kt
@@ -0,0 +1,28 @@
+package com.paularolim.grocerylist.api.features.user.main.routes
+
+import io.ktor.client.call.*
+import io.ktor.client.request.*
+import io.ktor.http.*
+import io.ktor.server.testing.*
+import kotlin.test.*
+
+class UserRoutesKtTest {
+    @Test
+    fun `Should return 201 on register`() = testApplication {
+        val requestBody = """
+            {
+                "name": "any_name",
+                "email": "any_email"
+            }
+        """.trimIndent()
+        val responseBody = """{"message":"Success message"}"""
+
+        val response = client.post("/users/register") {
+            contentType(ContentType.Application.Json)
+            setBody(requestBody)
+        }
+
+        assertEquals(HttpStatusCode.Created, response.status)
+        assertEquals(responseBody, response.body())
+    }
+}
