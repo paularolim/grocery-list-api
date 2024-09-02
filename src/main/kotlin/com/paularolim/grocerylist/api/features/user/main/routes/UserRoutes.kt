50ba1f9778616f8d1f816cb9451c297c891235c5
Paula Rolim <rolim.paula1999@gmail.com>
2024-09-02 18:33:06 -0300

    feat: use user register controller factory on routes

--- src/main/kotlin/com/paularolim/grocerylist/api/features/user/main/routes/UserRoutes.kt
+++ src/main/kotlin/com/paularolim/grocerylist/api/features/user/main/routes/UserRoutes.kt
@@ -1,15 +1,17 @@
 package com.paularolim.grocerylist.api.features.user.main.routes
 
-import com.paularolim.grocerylist.api.features.user.presentation.controllers.UserRegisterController
+import com.paularolim.grocerylist.api.features.user.main.factories.controllers.makeUserRegisterController
 import com.paularolim.grocerylist.api.plugins.routing.customResponse
 import io.ktor.server.application.*
+import io.ktor.server.request.*
 import io.ktor.server.routing.*
+import kotlinx.serialization.json.JsonObject
 
 fun Route.userRoutes() {
     route("/users") {
         post("/register") {
-            val controller = UserRegisterController().handle(call)
-            call.customResponse(controller)
+            val body = call.receive<JsonObject>()
+            call.customResponse(makeUserRegisterController().handle(body))
         }
     }
 }
