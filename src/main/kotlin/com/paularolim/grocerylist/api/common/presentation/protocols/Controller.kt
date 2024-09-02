c5f781063fe3d8441a2b1964073810973f51cd95
Paula Rolim <rolim.paula1999@gmail.com>
2024-09-02 18:27:47 -0300

    refactor: controller interface typo

--- src/main/kotlin/com/paularolim/grocerylist/api/common/presentation/protocols/Controller.kt
+++ src/main/kotlin/com/paularolim/grocerylist/api/common/presentation/protocols/Controller.kt
@@ -2,6 +2,6 @@ package com.paularolim.grocerylist.api.common.presentation.protocols
 
 import com.paularolim.grocerylist.api.utils.Response
 
-interface Controller {
-    fun handle(request: Any): Response
+interface Controller<T> {
+    suspend fun handle(request: T): Response
 }
