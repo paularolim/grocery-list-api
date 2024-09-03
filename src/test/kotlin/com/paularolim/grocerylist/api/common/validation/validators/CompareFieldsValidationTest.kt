1e390896bef769a36ef60784acb03ea62c575cdf
Paula Rolim <rolim.paula1999@gmail.com>
2024-09-03 20:40:17 -0300

    test: should return an null if fields match

--- src/test/kotlin/com/paularolim/grocerylist/api/common/validation/validators/CompareFieldsValidationTest.kt
+++ src/test/kotlin/com/paularolim/grocerylist/api/common/validation/validators/CompareFieldsValidationTest.kt
@@ -3,11 +3,12 @@ package com.paularolim.grocerylist.api.common.validation.validators
 import com.paularolim.grocerylist.api.common.presentation.errors.InvalidParamException
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertNull
 
 
 class CompareFieldsValidationTest {
     private val field = "anyField"
-    private val fieldToCompare = "anyField"
+    private val fieldToCompare = "anyOtherField"
 
     data class FakeInputForInvalidTest(
         val anyField: String,
@@ -23,5 +24,12 @@ class CompareFieldsValidationTest {
         val sut = makeSut()
         val error = sut.validate(FakeInputForInvalidTest("any_value_1", "any_value_2"))
         assertEquals(InvalidParamException(fieldToCompare).message, error?.message)
+    }
+
+    @Test
+    fun `should return an null if fields match`() {
+        val sut = makeSut()
+        val result = sut.validate(FakeInputForInvalidTest("any_value", "any_value"))
+        assertNull(result)
     }
 }
