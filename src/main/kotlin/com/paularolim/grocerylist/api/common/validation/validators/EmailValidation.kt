aee5016e9d53523b7745642d5dbe72fe1646d448
Paula Rolim <rolim.paula1999@gmail.com>
2024-09-03 20:14:11 -0300

    fix: email regex

--- src/main/kotlin/com/paularolim/grocerylist/api/common/validation/validators/EmailValidation.kt
+++ src/main/kotlin/com/paularolim/grocerylist/api/common/validation/validators/EmailValidation.kt
@@ -7,13 +7,13 @@ import kotlin.reflect.full.memberProperties
 class EmailValidation<T : Any>(
     private val fieldName: String
 ) : Validation<T> {
-    private val emailRegex = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\$"
+    private val emailRegex = Regex("^[\\w-.]+@([\\w-]+\\.)+[\\w-]{2,4}$")
 
     override fun validate(input: T): Exception? {
         val property = input::class.memberProperties.find { it.name == fieldName }
         if (property != null) {
             val value = property.call(input)
-            if (value != null && value is String && !value.matches(emailRegex.toRegex())) {
+            if (value != null && value is String && !value.matches(emailRegex)) {
                 return InvalidParamException(fieldName)
             }
         }
