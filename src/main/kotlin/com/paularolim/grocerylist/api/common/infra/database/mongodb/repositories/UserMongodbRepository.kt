ba1e51bd926e550eeab2176dd1e5c4ebd0ea549c
Paula Rolim <rolim.paula1999@gmail.com>
2024-09-03 08:49:22 -0300

    feat: user register mongodb repository implementation

---
+++ src/main/kotlin/com/paularolim/grocerylist/api/common/infra/database/mongodb/repositories/UserMongodbRepository.kt
@@ -0,0 +1,15 @@
+package com.paularolim.grocerylist.api.common.infra.database.mongodb.repositories
+
+import com.paularolim.grocerylist.api.common.infra.database.mongodb.connection.MongodbConnection
+import com.paularolim.grocerylist.api.common.infra.database.mongodb.models.User
+import com.paularolim.grocerylist.api.features.user.data.protocols.UserRegisterRepository
+import com.paularolim.grocerylist.api.features.user.domain.usecases.UserRegisterUsecase
+
+class UserMongodbRepository : UserRegisterRepository {
+    override suspend fun register(data: UserRegisterUsecase.RegisterParams): Boolean {
+        val collection = MongodbConnection.getCollection<User>("users")
+        val user = User(data.name, data.email)
+        val result = collection.insertOne(user)
+        return result.insertedId != null
+    }
+}
