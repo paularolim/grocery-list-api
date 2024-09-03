26204ded3bdbf5ce4a9cdf4c85d75f4b86fe0e95
Paula Rolim <rolim.paula1999@gmail.com>
2024-09-03 20:40:10 -0300

    feat: should return an null if fields match

--- src/main/kotlin/com/paularolim/grocerylist/api/common/validation/validators/CompareFieldsValidation.kt
+++ src/main/kotlin/com/paularolim/grocerylist/api/common/validation/validators/CompareFieldsValidation.kt
@@ -9,11 +9,15 @@ class CompareFieldsValidation<T : Any>(
     private val fieldToCompare: String
 ) : Validation<T> {
     override fun validate(input: T): Exception? {
-        val property = input::class.memberProperties.find { it.name == fieldName }
-        if (property != null) {
-            val value = property.call(input)
-            if (value != null && value is String && value != fieldToCompare) {
-                return InvalidParamException(fieldName)
+        val property1 = input::class.memberProperties.find { it.name == fieldName }
+        val property2 = input::class.memberProperties.find { it.name == fieldToCompare }
+        if (property1 != null && property2 != null) {
+            val value1 = property1.call(input)
+            val value2 = property2.call(input)
+            val value1IsValid = value1 != null && value1 is String
+            val value2IsValid = value2 != null && value2 is String
+            if (value1IsValid && value2IsValid && value1 != value2) {
+                return InvalidParamException(fieldToCompare)
             }
         }
         return null
