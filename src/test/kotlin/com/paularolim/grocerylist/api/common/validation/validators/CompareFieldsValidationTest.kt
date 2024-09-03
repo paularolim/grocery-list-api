6a892e64a7f79520f16c21bd1ca160589144aa8b
Paula Rolim <rolim.paula1999@gmail.com>
2024-09-03 20:44:32 -0300

    test: should call CompareFieldsValidation with correct values

--- src/test/kotlin/com/paularolim/grocerylist/api/common/validation/validators/CompareFieldsValidationTest.kt
+++ src/test/kotlin/com/paularolim/grocerylist/api/common/validation/validators/CompareFieldsValidationTest.kt
@@ -1,6 +1,10 @@
 package com.paularolim.grocerylist.api.common.validation.validators
 
 import com.paularolim.grocerylist.api.common.presentation.errors.InvalidParamException
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.slot
+import io.mockk.verify
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertNull
@@ -10,7 +14,7 @@ class CompareFieldsValidationTest {
     private val field = "anyField"
     private val fieldToCompare = "anyOtherField"
 
-    data class FakeInputForInvalidTest(
+    data class FakeInputForTest(
         val anyField: String,
         val anyOtherField: String
     )
@@ -22,14 +26,30 @@ class CompareFieldsValidationTest {
     @Test
     fun `should return an error if fields do not match`() {
         val sut = makeSut()
-        val error = sut.validate(FakeInputForInvalidTest("any_value_1", "any_value_2"))
+        val error = sut.validate(FakeInputForTest("any_value_1", "any_value_2"))
         assertEquals(InvalidParamException(fieldToCompare).message, error?.message)
     }
 
     @Test
     fun `should return an null if fields match`() {
         val sut = makeSut()
-        val result = sut.validate(FakeInputForInvalidTest("any_value", "any_value"))
+        val result = sut.validate(FakeInputForTest("any_value", "any_value"))
         assertNull(result)
+    }
+
+    @Test
+    fun `should call CompareFieldsValidation with correct values`() {
+        val input = FakeInputForTest("any_value_1", "any_value_2")
+
+        val compareFieldsValidation = mockk<CompareFieldsValidation<FakeInputForTest>>(relaxed = true)
+
+        val capturedInput = slot<FakeInputForTest>()
+
+        every { compareFieldsValidation.validate(capture(capturedInput)) } returns null
+
+        compareFieldsValidation.validate(input)
+        verify { compareFieldsValidation.validate(input) }
+
+        assertEquals(input, capturedInput.captured)
     }
 }
