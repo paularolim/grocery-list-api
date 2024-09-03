ed10953658562fb398d028c74465058a1d94a16e
Paula Rolim <rolim.paula1999@gmail.com>
2024-09-03 13:30:07 -0300

    test: should call EmailValidator with correct email

--- src/test/kotlin/com/paularolim/grocerylist/api/common/validation/validators/EmailValidationTest.kt
+++ src/test/kotlin/com/paularolim/grocerylist/api/common/validation/validators/EmailValidationTest.kt
@@ -1,17 +1,22 @@
 package com.paularolim.grocerylist.api.common.validation.validators
 
 import com.paularolim.grocerylist.api.common.presentation.errors.InvalidParamException
+import io.mockk.*
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class EmailValidationTest {
     private val field = "anyField"
 
+    data class FakeInputForValidTest(
+        val anyField: String
+    )
+
     data class FakeInputForInvalidTest(
         val anyField: String
     )
 
-    private fun makeSut(): EmailValidation<FakeInputForInvalidTest> {
+    private fun makeSut(): EmailValidation<Any> {
         return EmailValidation(field)
     }
 
@@ -21,5 +26,21 @@ class EmailValidationTest {
         val email = "any_invalid_email"
         val error = sut.validate(FakeInputForInvalidTest(email))
         assertEquals(InvalidParamException(field).message, error?.message)
+    }
+
+    @Test
+    fun `should call EmailValidator with correct email`() {
+        val input = FakeInputForValidTest("valid_email@example.com")
+
+        val emailValidation = mockk<EmailValidation<FakeInputForValidTest>>(relaxed = true)
+
+        val capturedInput = slot<FakeInputForValidTest>()
+
+        every { emailValidation.validate(capture(capturedInput)) } returns null
+
+        emailValidation.validate(input)
+        verify { emailValidation.validate(input) }
+
+        assertEquals(input, capturedInput.captured)
     }
 }
