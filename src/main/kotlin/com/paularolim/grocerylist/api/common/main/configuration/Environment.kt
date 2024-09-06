3f6b8600119ab669419b0399fa5249664db24f9c
Paula Rolim <rolim.paula1999@gmail.com>
2024-09-06 18:35:28 -0300

    refactor: change environment from class to object

--- src/main/kotlin/com/paularolim/grocerylist/api/common/main/configuration/Environment.kt
+++ src/main/kotlin/com/paularolim/grocerylist/api/common/main/configuration/Environment.kt
@@ -2,16 +2,14 @@ package com.paularolim.grocerylist.api.common.main.configuration
 
 import io.github.cdimascio.dotenv.Dotenv
 
-class Environment {
-    companion object {
-        private val env = System.getenv("KTOR_ENV") ?: "development"
+object Environment {
+    private val env = System.getenv("KTOR_ENV") ?: "development"
 
-        private fun load(): Dotenv? {
-            return Dotenv.configure().filename(".env.$env").load()
-        }
+    private fun load(): Dotenv? {
+        return Dotenv.configure().filename(".env.$env").load()
+    }
 
-        fun get(name: String): String {
-            return load()?.get(name) ?: ""
-        }
+    fun get(name: String): String {
+        return load()?.get(name) ?: ""
     }
 }
