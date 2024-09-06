dac7ae82caaa65851c8dcff72d050caf3e09d9a6
Paula Rolim <rolim.paula1999@gmail.com>
2024-09-06 19:09:34 -0300

    feat: add bcrypt implementation

--- src/main/kotlin/com/paularolim/grocerylist/api/features/user/data/usecases/DatabaseUserRegisterUsecase.kt
+++ src/main/kotlin/com/paularolim/grocerylist/api/features/user/data/usecases/DatabaseUserRegisterUsecase.kt
@@ -1,13 +1,20 @@
 package com.paularolim.grocerylist.api.features.user.data.usecases
 
+import com.paularolim.grocerylist.api.common.data.protocols.cryptography.Hasher
 import com.paularolim.grocerylist.api.features.user.data.protocols.UserRegisterRepository
 import com.paularolim.grocerylist.api.features.user.domain.usecases.UserRegisterUsecase
 
 class DatabaseUserRegisterUsecase(
-    private val repository: UserRegisterRepository
+    private val repository: UserRegisterRepository,
+    private val hasher: Hasher
 ) : UserRegisterUsecase {
     override suspend fun handle(user: UserRegisterUsecase.RegisterParams): Boolean {
-        return repository.register(user)
+        val hashedPassword = this.hasher.hash(user.password)
+        return repository.register(UserRegisterUsecase.RegisterParams(
+            name = user.name,
+            email = user.email,
+            password = hashedPassword,
+        ))
     }
 
 }
--- src/main/kotlin/com/paularolim/grocerylist/api/features/user/main/factories/usecases/makeUserRegisterUsecase.kt
+++ src/main/kotlin/com/paularolim/grocerylist/api/features/user/main/factories/usecases/makeUserRegisterUsecase.kt
@@ -1,10 +1,13 @@
 package com.paularolim.grocerylist.api.features.user.main.factories.usecases
 
+import com.paularolim.grocerylist.api.common.infra.cryptography.BcryptAdapter
 import com.paularolim.grocerylist.api.common.infra.database.mongodb.repositories.UserMongodbRepository
 import com.paularolim.grocerylist.api.features.user.data.usecases.DatabaseUserRegisterUsecase
 import com.paularolim.grocerylist.api.features.user.domain.usecases.UserRegisterUsecase
 
 fun makeUserRegisterUsecase(): UserRegisterUsecase {
     val repository = UserMongodbRepository()
-    return DatabaseUserRegisterUsecase(repository)
+    val salt = 12
+    val hasher = BcryptAdapter(salt)
+    return DatabaseUserRegisterUsecase(repository, hasher)
 }
