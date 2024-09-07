130ef52aa7ec38a07633282a3828c78e94a0bde7
Paula Rolim <rolim.paula1999@gmail.com>
2024-09-06 22:48:07 -0300

    feat: hash comparer implementation

--- src/main/kotlin/com/paularolim/grocerylist/api/common/infra/cryptography/BcryptAdapter.kt
+++ src/main/kotlin/com/paularolim/grocerylist/api/common/infra/cryptography/BcryptAdapter.kt
@@ -1,12 +1,17 @@
 package com.paularolim.grocerylist.api.common.infra.cryptography
 
 import at.favre.lib.crypto.bcrypt.BCrypt
+import com.paularolim.grocerylist.api.common.data.protocols.cryptography.HashComparer
 import com.paularolim.grocerylist.api.common.data.protocols.cryptography.Hasher
 
 class BcryptAdapter(
     private val salt: Int
-) : Hasher {
+) : Hasher, HashComparer {
     override fun hash(plainText: String): String {
         return BCrypt.withDefaults().hashToString(salt, plainText.toCharArray())
+    }
+
+    override fun compare(plainText: String, digest: String): Boolean {
+        return BCrypt.verifyer().verify(plainText.toCharArray(), digest.toCharArray()).verified
     }
 }
