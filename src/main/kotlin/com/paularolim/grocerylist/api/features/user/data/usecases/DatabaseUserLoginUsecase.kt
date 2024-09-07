4a8d74484f6ac521829bcb1a47c130bca602bc32
Paula Rolim <rolim.paula1999@gmail.com>
2024-09-06 22:49:29 -0300

    feat: implement hash comparer on usecase

--- src/main/kotlin/com/paularolim/grocerylist/api/features/user/data/usecases/DatabaseUserLoginUsecase.kt
+++ src/main/kotlin/com/paularolim/grocerylist/api/features/user/data/usecases/DatabaseUserLoginUsecase.kt
@@ -1,16 +1,20 @@
 package com.paularolim.grocerylist.api.features.user.data.usecases
 
+import com.paularolim.grocerylist.api.common.data.protocols.cryptography.HashComparer
 import com.paularolim.grocerylist.api.features.user.data.protocols.UserByEmailRepository
 import com.paularolim.grocerylist.api.features.user.domain.usecases.UserLoginUsecase
 
 class DatabaseUserLoginUsecase(
     private val userByEmailRepository: UserByEmailRepository,
+    private val hashComparer: HashComparer
 ): UserLoginUsecase {
     override suspend fun handle(params: UserLoginUsecase.Params): UserLoginUsecase.Result? {
         val userFound = this.userByEmailRepository.getByEmail(params.email)
         if (userFound != null) {
-            TODO("check password")
-            return null
+            val isValid = this.hashComparer.compare(params.password, userFound.password)
+            if (isValid) {
+                TODO("generate token")
+            }
         }
         return null
     }
--- src/main/kotlin/com/paularolim/grocerylist/api/features/user/main/factories/usecases/makeUserLoginUsecase.kt
+++ src/main/kotlin/com/paularolim/grocerylist/api/features/user/main/factories/usecases/makeUserLoginUsecase.kt
@@ -1,10 +1,13 @@
 package com.paularolim.grocerylist.api.features.user.main.factories.usecases
 
+import com.paularolim.grocerylist.api.common.infra.cryptography.BcryptAdapter
 import com.paularolim.grocerylist.api.common.infra.database.mongodb.repositories.UserMongodbRepository
 import com.paularolim.grocerylist.api.features.user.data.usecases.DatabaseUserLoginUsecase
 import com.paularolim.grocerylist.api.features.user.domain.usecases.UserLoginUsecase
 
 fun makeUserLoginUsecase(): UserLoginUsecase {
     val repository = UserMongodbRepository()
-    return DatabaseUserLoginUsecase(repository)
+    val salt = 12
+    val hasher = BcryptAdapter(salt)
+    return DatabaseUserLoginUsecase(repository, hasher)
 }
