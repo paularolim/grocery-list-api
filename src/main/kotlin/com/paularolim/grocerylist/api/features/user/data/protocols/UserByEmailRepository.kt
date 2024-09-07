f780526a701341cce8707cd17424f09af08a5b46
Paula Rolim <rolim.paula1999@gmail.com>
2024-09-06 21:53:19 -0300

    feat: add user by email interface

---
+++ src/main/kotlin/com/paularolim/grocerylist/api/features/user/data/protocols/UserByEmailRepository.kt
@@ -0,0 +1,11 @@
+package com.paularolim.grocerylist.api.features.user.data.protocols
+
+interface UserByEmailRepository {
+    data class Result (
+        val id: String,
+        val name: String,
+        val email: String,
+    )
+
+    suspend fun getByEmail(email: String) : Result?
+}
