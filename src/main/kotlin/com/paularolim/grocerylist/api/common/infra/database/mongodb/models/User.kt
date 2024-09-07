9cd6740f3ca4eb33d0d788d05becb441e3375d31
Paula Rolim <rolim.paula1999@gmail.com>
2024-09-06 21:54:04 -0300

    feat: implement user by email on database

--- src/main/kotlin/com/paularolim/grocerylist/api/common/infra/database/mongodb/models/User.kt
+++ src/main/kotlin/com/paularolim/grocerylist/api/common/infra/database/mongodb/models/User.kt
@@ -1,7 +1,25 @@
 package com.paularolim.grocerylist.api.common.infra.database.mongodb.models
 
-data class User(
+import org.bson.codecs.pojo.annotations.BsonId
+import org.bson.types.ObjectId
+
+data class UserRegister(
     val name: String,
     val email: String,
     val password: String,
+)
+
+data class User(
+    @BsonId
+    val id: ObjectId,
+    val name: String,
+    val email: String,
+    val password: String
+)
+
+data class UserResult(
+    @BsonId
+    val id: ObjectId,
+    val name: String,
+    val email: String
 )
--- src/main/kotlin/com/paularolim/grocerylist/api/common/infra/database/mongodb/repositories/UserMongodbRepository.kt
+++ src/main/kotlin/com/paularolim/grocerylist/api/common/infra/database/mongodb/repositories/UserMongodbRepository.kt
@@ -1,15 +1,45 @@
 package com.paularolim.grocerylist.api.common.infra.database.mongodb.repositories
 
 import com.paularolim.grocerylist.api.common.infra.database.mongodb.connection.MongodbConnection
-import com.paularolim.grocerylist.api.common.infra.database.mongodb.models.User
+import com.paularolim.grocerylist.api.features.user.data.protocols.UserByEmailRepository
 import com.paularolim.grocerylist.api.features.user.data.protocols.UserRegisterRepository
 import com.paularolim.grocerylist.api.features.user.domain.usecases.UserRegisterUsecase
+import com.mongodb.client.model.Filters.eq
+import com.mongodb.client.model.Projections
+import com.paularolim.grocerylist.api.common.infra.database.mongodb.models.User
+import com.paularolim.grocerylist.api.common.infra.database.mongodb.models.UserRegister
+import com.paularolim.grocerylist.api.common.infra.database.mongodb.models.UserResult
+import kotlinx.coroutines.flow.firstOrNull
 
-class UserMongodbRepository : UserRegisterRepository {
+
+
+class UserMongodbRepository : UserRegisterRepository, UserByEmailRepository {
     override suspend fun register(data: UserRegisterUsecase.RegisterParams): Boolean {
-        val collection = MongodbConnection.getCollection<User>("users")
-        val user = User(data.name, data.email, data.password)
+        val collection = MongodbConnection.getCollection<UserRegister>("users")
+        val user = UserRegister(data.name, data.email, data.password)
         val result = collection.insertOne(user)
+        MongodbConnection.close()
         return result.insertedId != null
+    }
+
+    override suspend fun getByEmail(email: String): UserByEmailRepository.Result? {
+        val collection = MongodbConnection.getCollection<User>("users")
+        val projectionFields= Projections.fields(
+            Projections.include(User::name.name, User::email.name),
+        )
+        val resultsFlow = collection.withDocumentClass<UserResult>()
+            .find(eq(User::email.name, email))
+            .projection(projectionFields)
+            .firstOrNull()
+        MongodbConnection.close()
+        return if (resultsFlow == null) {
+            null
+        } else {
+            UserByEmailRepository.Result(
+                id = resultsFlow.id.toString(),
+                name = resultsFlow.name,
+                email = resultsFlow.email
+            )
+        }
     }
 }
